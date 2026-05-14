739200fc24dddcfb9c285e6436f8caef77ac3755
Xavier Kania <xman3939@gmail.com>
2026-05-13 23:17:19 -0500

    loader test

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const loader = document.getElementById('loader');
+const home = document.getElementById('home');
+
+const loaderDuration = 1800;
+const blackPauseDuration = 450;
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    // Fade loader out into pure black.
+    loader.classList.add('is-hidden');
+
+    // After black pause, fade in real homepage.
+    setTimeout(() => {
+      home.classList.add('is-visible');
+    }, 900 + blackPauseDuration);
+  }, loaderDuration);
+});
